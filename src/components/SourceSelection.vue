d67560677b588a52cdb8f7a958793be22c3b017b
cokoyoh <charlesokoyoh@gmail.com>
2017-11-20 19:52:45 +0300

    api request added

--- src/components/SourceSelection.vue
+++ src/components/SourceSelection.vue
@@ -14,6 +14,23 @@
                 sources: [],
                 source: ''
             }
+        },
+        methods:{
+            sourceChanged(e){
+                console.log("Source changed method called here");
+                for(let i = 0 ; i <= this.sources.length; i++){
+                    if(this.sources[i].id == e.target.value){
+                        this.source == this.sources[i];
+                    }
+                }
+            }
+        },
+        created(){
+            console.log("Source selection created, api called here");
+            this.$http.get('https://newsapi.org/v1/sources?language=en')
+                .then(response => {
+                    this.sources = response.data.sources;
+                })
         }
     }
 
